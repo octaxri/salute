e46d96e4debc6795bf4bcfd1e2651176239a3931
Dhimas Putranto Budhi Harto <dhimasputrabudhi@gmail.com>
2019-11-23 23:38:10 +0700

    Assets move

--- application/models/M_Kuisoner_B.php
+++ application/models/M_Kuisoner_B.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class M_Kuisoner_B extends CI_Model {
 
 	
 	public function index()
--- application/models/M_Login.php
+++ application/models/M_Login.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class M_Login extends CI_Model {
 
 	
 	public function index()
