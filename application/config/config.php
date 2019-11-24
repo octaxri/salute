4b67d5a827a1fbbe308f6a04d8b0f7a595d56cb5
Dhimas Putranto Budhi Harto <dhimasputrabudhi@gmail.com>
2019-11-24 15:18:21 +0700

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CI_SIAP_PAKAI/';
+$config['base_url'] = 'http://localhost/salute/';
 
 /*
 |--------------------------------------------------------------------------
