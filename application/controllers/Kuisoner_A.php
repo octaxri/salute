bd7d01538213bb0a7c2576ac2574edb69e273f12
Dhimas Putranto Budhi Harto <dhimasputrabudhi@gmail.com>
2019-12-01 21:06:37 +0700

    Kuisoner A

--- application/controllers/Kuisoner_A.php
+++ application/controllers/Kuisoner_A.php
@@ -2,16 +2,63 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Kuisoner_A extends CI_Controller {
-
 	
+	function __construct() 
+	{
+		parent::__construct();
+		$this->load->model('m_Kuisoner_A');
+		
+	}
+
 	public function index()
 	{
-		$data['title'] = "Kuisoner A";
+		$data['title'] = " SALUTE | Kuisoner A";
+		$data['data']=$this->m_Kuisoner_A->tampil_kuisoner();
+
+		$data['user'] = $this->db->get_where('user', ['username' =>
+		$this->session->userdata('username')])->row_array();
+		
 		$this->load->view('templates/header',$data);
 		$this->load->view('templates/sidebar',$data);
 		$this->load->view('v_kuisoner/kuisoner_A',$data);
 		$this->load->view('templates/footer');
-    }
+	}
+
+	public function tambah_kuisoner()
+	{
+		$this->m_Kuisoner_A->tambah_kuisoner();
+		$this->session->set_flashdata('msg', 'Berhasil Ditambahkan');
+		redirect('kuisoner_a');
+		
+		
+	}
+	
+	public function edit_kuisoner()
+	{
+		$data=array(
+
+			"soalA" => $_POST['soalA'],
+			"jawaban1A" => $_POST['jawaban1A'],
+			"jawaban2A" => $_POST['jawaban2A'],
+			"jawaban3A" => $_POST['jawaban3A'],
+			"jawaban4A" => $_POST['jawaban4A']
+		);
+
+		$this->db->where('id_kuisionerA', $_POST['id_kuisionerA']);
+		$this->db->update('kuisioner_a', $data);
+		$this->session->set_flashdata('msg', 'Data Berhasil Di Edit');
+		redirect('kuisoner_a');
+		
+	}
+
+
+	public function hapus_kuisoner()
+	{
+		$id= $this->input->post('id_kuisionerA');
+		$this->m_Kuisoner_A->hapus_kuisoner($id);
+		$this->session->set_flashdata('msg', 'Data Berhasil Di Hapus');
+		redirect('kuisoner_a');
+	}
     
     
 }
--- application/models/M_Kuisoner_A.php
+++ application/models/M_Kuisoner_A.php
@@ -1,13 +1,40 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_Kuisoner_A extends CI_Model {
+class M_Kuisoner_A extends CI_Model 
+{
 
 	
-	public function index()
+	public function tampil_kuisoner()
 	{
-		$this->load->view('welcome_message');
-    }
+		$this->db->select('*');
+		$this->db->from('kuisioner_a');
+		$query= $this->db->get()->result_array();
+
+		return $query;
+			
+	}
+
+	public function tambah_kuisoner()
+	{
+		$data=array(
+
+			"soalA" => $this->input->post('soalA',TRUE),
+			"jawaban1A" => $this->input->post('jawaban1A',TRUE),
+			"jawaban2A" => $this->input->post('jawaban2A',TRUE),
+			"jawaban3A" => $this->input->post('jawaban3A',TRUE),
+			"jawaban4A" => $this->input->post('jawaban4A',TRUE)
+		);
+
+		$this->db->insert('kuisioner_a', $data);
+		
+	}
+
+	public function hapus_kuisoner($id)
+	{
+		$this->db->delete('kuisioner_a',['id_kuisionerA' => $id]);
+		
+	}
     
     
 }
