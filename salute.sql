10f71e9c915cfed1dc57439bb1336e5d5d449cc0
Dhimas Putranto Budhi Harto <dhimasputrabudhi@gmail.com>
2019-12-06 22:43:53 +0700

    db baru

--- salute.sql
+++ salute.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2019 at 08:17 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Generation Time: Dec 06, 2019 at 04:43 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -85,7 +85,10 @@ CREATE TABLE `detail_peserta` (
 
 INSERT INTO `detail_peserta` (`id`, `kd_pelatihan`, `id_user`) VALUES
 (3, '2019-00000001', 4),
-(5, '2019-00000002', 4);
+(5, '2019-00000002', 4),
+(6, '2019-00000002', 5),
+(7, '2019-00000003', 5),
+(8, '2019-00000003', 6);
 
 -- --------------------------------------------------------
 
@@ -154,10 +157,36 @@ CREATE TABLE `kuisioner_b` (
 --
 
 INSERT INTO `kuisioner_b` (`id_kuisionerB`, `jenis_soal`, `soalB`, `jawaban1B`, `jawaban2B`, `jawaban3B`, `jawaban4B`, `jawaban5B`, `tipe_soal`, `sub_soal`) VALUES
-(26, 1, 'Tulisan di dalam materi pelatihan jelas dan mudah di baca', 5, 4, 3, 2, 1, 'pg', 1),
-(27, 1, 'Kualitas materi pelatihan dapat menambah tingkat ketrampilan dan pengetahuan anda', 5, 4, 3, 2, 1, 'pg', 2),
-(28, 1, ' Tahapan materi pelatihan sudah berurutan dari materi tingkat dasar sampai dengan materi tingkat lanjut', 5, 4, 3, 2, 1, 'pg', 1),
-(29, 1, 'Komentar / saran tentang materi pelatihan :', 0, 0, 0, 0, 0, 'uraian', 0);
+(26, 1, 'Tulisan di dalam materi pelatihan jelas dan mudah di baca', 5, 4, 3, 2, 1, 'pg', 4),
+(27, 1, 'Kualitas materi pelatihan dapat menambah tingkat ketrampilan dan pengetahuan anda', 5, 4, 3, 2, 1, 'pg', 4),
+(28, 1, ' Tahapan materi pelatihan sudah berurutan dari materi tingkat dasar sampai dengan materi tingkat lanjut', 5, 4, 3, 2, 1, 'pg', 4),
+(29, 1, 'Komentar / saran tentang materi pelatihan :', 0, 0, 0, 0, 0, 'uraian', 8),
+(30, 3, 'Workshop yang ada telah memiliki kelengkapan alat/mesin untuk praktek dengan jumlah yang cukup', 1, 2, 3, 4, 5, 'pg', 1),
+(31, 3, 'Peralatan dan mesin di workshop dalam kondisi baik dan siap pakai', 1, 2, 3, 4, 5, 'pg', 1),
+(32, 3, 'Workshop dilengkapi intruksi & prosedur cara penggunaan alat/mesin', 1, 2, 3, 4, 5, 'pg', 1),
+(33, 3, 'Kelengkapan P3K di workshop tersedia', 1, 2, 3, 4, 5, 'pg', 1),
+(34, 3, 'Kelengkapan alat pelindung diri tersedia', 1, 2, 3, 4, 5, 'pg', 1),
+(35, 3, 'Kelengkapan alat kebersihan tersedia dan kondisi baik', 1, 2, 3, 4, 5, 'pg', 1),
+(36, 3, 'Kondisi ruang teori dalam keadaan baik, nyaman dan bersih', 1, 2, 3, 4, 5, 'pg', 2),
+(37, 3, 'Diruang teori tersedia alat/media pelatihan dalam kondisi baik', 1, 2, 3, 4, 5, 'pg', 2),
+(38, 3, 'Meja dan kursi bagi instruktur dan peserta tersedia dalam kondisi baik dan cukup', 1, 2, 3, 4, 5, 'pg', 2),
+(39, 3, 'Kelengkapan alat kebersihan tersedia dan kondisi baik', 1, 2, 3, 4, 5, 'pg', 2),
+(40, 3, 'Sumber listrik untuk peralatan pelatihan dalam keadaan cukup', 1, 2, 3, 4, 5, 'pg', 5),
+(41, 3, 'Penerangan lampu pada ruangan pelatihan dan bengkel dalam kondisi cukup dan baik', 1, 2, 3, 4, 5, 'pg', 5),
+(42, 3, ' Air bersih cukup tersedia', 1, 2, 3, 4, 5, 'pg', 6),
+(43, 3, 'Kamar mandi/toilet dalam kondisi bersih, wangi dan tidak licin', 1, 2, 3, 4, 5, 'pg', 6),
+(44, 3, 'Kran yang terpasang kondisinya baik', 1, 2, 3, 4, 5, 'pg', 6),
+(45, 3, 'Perlengkapan kamar mandi dan toilet tersedia', 1, 2, 3, 4, 5, 'pg', 6),
+(46, 3, 'Sarana ibadah bersih dan dilengkapi dengan perlengkapan ibadah', 1, 2, 3, 4, 5, 'pg', 7),
+(47, 3, 'Sarana olah raga yang memadai', 1, 2, 3, 4, 5, 'pg', 7),
+(48, 3, 'Layanan kesehatan yang memadai', 1, 2, 3, 4, 5, 'pg', 7),
+(49, 3, 'Perpustakaan berisi buku-buku penunjang pelatihan', 1, 2, 3, 4, 5, 'pg', 7),
+(50, 3, 'Komentar / saran tentang Sarana Prasarana :', 0, 0, 0, 0, 0, 'uraian', 8),
+(51, 4, 'Ketersediaan bahan latihan yang digunakan', 1, 2, 3, 4, 5, 'pg', 4),
+(52, 4, 'Modul pelatihan yang diterimakan peserta (secara umum)', 1, 2, 3, 4, 5, 'pg', 4),
+(53, 4, 'Alat tulis / kelengkapan yang diterima peserta', 1, 2, 3, 4, 5, 'pg', 4),
+(54, 4, 'Seragam yang diterima peserta (kondisi umum : ukuran, jenis bahan , kenyamanan dalam pemakaian)', 1, 2, 3, 4, 5, 'pg', 4),
+(55, 4, 'Komentar / saran tentang Bahan Latihan, Modul, ATK, dan Seragam Peserta :', 0, 0, 0, 0, 0, 'uraian', 8);
 
 -- --------------------------------------------------------
 
@@ -181,13 +210,45 @@ CREATE TABLE `kuisioner_c` (
 --
 
 INSERT INTO `kuisioner_c` (`id_kuisionerC`, `jenis_soal`, `soalC`, `jawaban1C`, `jawaban2C`, `jawaban3C`, `jawaban4C`, `tipe_soal`) VALUES
-(7, 1, 'da', 1, 2, 3, 4, 'pg'),
-(8, 2, 'qw', 1, 2, 3, 4, 'pg'),
-(10, 3, 'qre', 1, 2, 3, 4, 'pg'),
-(11, 4, 'sese', 1, 2, 3, 4, 'pg'),
-(12, 5, 'sd', 1, 2, 3, 4, 'pg'),
-(13, 6, 'rere', 1, 2, 3, 4, 'pg'),
-(14, 7, 'polken', 1, 2, 3, 4, 'pg');
+(7, 1, 'Secara umum Pelaksanaan rekruitmen di daerah asal oleh petugas rekruit.', 1, 2, 3, 4, 'pg'),
+(15, 1, 'Pemberitahuan / informasi tentang program pelatihan boarding yang diterima oleh peserta.', 1, 2, 3, 4, 'pg'),
+(16, 1, 'Kemudahan dalam memenuhi persyaratan yang harus dipenuhi calon peserta.', 1, 2, 3, 4, 'pg'),
+(17, 1, 'Perjalanan dari daerah asal peserta ke Bandara asal peserta.', 1, 2, 3, 4, 'pg'),
+(18, 1, 'Perjalanan dari bandara Semarang ke BBPLK Semarang.', 1, 2, 3, 4, 'pg'),
+(19, 1, 'Akomodasi dan konsumsi yang diterima selama perjalanan.', 1, 2, 3, 4, 'pg'),
+(20, 1, 'Kenyamanan selama perjalanan.', 1, 2, 3, 4, 'pg'),
+(21, 1, 'Yang berkenaan dengan rekruitmen peserta', 0, 0, 0, 0, 'uraian'),
+(22, 1, 'Yang berkenaan dengan perjalanan dari daerah asal ke BBPLK Semarang peserta', 0, 0, 0, 0, 'uraian'),
+(23, 1, 'Yang berkenaan dengan persyaratan yang harus dipenuhi peserta.', 0, 0, 0, 0, 'uraian'),
+(24, 2, 'Penerimaan peserta setelah sampai di BBPLK Semarang (oleh security atau petugas asrama)', 1, 2, 3, 4, 'pg'),
+(25, 2, 'Pembagian kamar sudah sesuai dengan keinginan peserta', 1, 2, 3, 4, 'pg'),
+(26, 2, 'Ada petugas yang mengarahkan pada saat peserta sampai di asrama', 1, 2, 3, 4, 'pg'),
+(27, 2, 'Keramahan petugas pada saat kedatangan peserta di BBPLK Semarang', 1, 2, 3, 4, 'pg'),
+(28, 2, 'Yang berkenaan dengan penyambutan peserta', 0, 0, 0, 0, 'uraian'),
+(29, 2, 'Yang berkenaan dengan pembagian kamar', 0, 0, 0, 0, 'uraian'),
+(30, 3, 'Kondisi kamar menurut anda (jumlah bed, penataan, kebersihan, tata udara, pencahayaan)', 1, 2, 3, 4, 'pg'),
+(31, 3, 'Kondisi kamar mandi (kebersihan, air, wc, tata udara, pencahayaan)', 1, 2, 3, 4, 'pg'),
+(32, 3, 'Kondisi asrama secara umum', 1, 2, 3, 4, 'pg'),
+(33, 3, 'Kondisi ruang kelas (kebersihan, tata udara, pencahayaan)', 1, 2, 3, 4, 'pg'),
+(34, 3, 'Kondisi lingkungan secara umum', 1, 2, 3, 4, 'pg'),
+(35, 3, 'Yang berkenaan dengan sarana dan prasarana', 0, 0, 0, 0, 'uraian'),
+(36, 4, 'Variasi menu yang disajikan', 1, 2, 3, 4, 'pg'),
+(37, 4, 'Cita rasa menu secara umum', 1, 2, 3, 4, 'pg'),
+(38, 4, 'Kebersihan dalam proses pengolahan sampai penyajian', 1, 2, 3, 4, 'pg'),
+(39, 4, 'Cara penyajian', 1, 2, 3, 4, 'pg'),
+(40, 4, 'Kebersihan alat makan\r\n', 1, 2, 3, 4, 'pg'),
+(41, 4, 'Kondisi ruang makan (kebersihan, tata udara, pencahayaan)', 1, 2, 3, 4, 'pg'),
+(42, 4, 'Jumlah makanan yang dihidangkan', 1, 2, 3, 4, 'pg'),
+(43, 4, 'Yang berkenaan dengan konsumsi', 0, 0, 0, 0, 'uraian'),
+(44, 5, 'Ketersediaan bahan latihan yang digunakan', 1, 2, 3, 4, 'pg'),
+(45, 5, 'Modul pelatihan yang diterimakan peserta (secara umum)', 1, 2, 3, 4, 'pg'),
+(46, 5, 'Alat tulis / kelengkapan yang diterima peserta', 1, 2, 3, 4, 'pg'),
+(47, 5, 'Seragam yang diterima peserta. (kondisi umum : ukuran, jenis bahan, kenyamanan dalam pemakaian)', 1, 2, 3, 4, 'pg'),
+(48, 5, 'Yang berkenaan dengan bahan latihan, modul, ATK, dan seragam peserta', 0, 0, 0, 0, 'uraian'),
+(49, 6, 'Secara umum pelaksanaan Uji Kompetensi yang dilaksanakan setelah mengikuti pelatihan (Kesesuaian materi uji, kemudahan dalam mengerjakan soal, waktu pengerjaan soal, kesiapan dalam UJK)', 1, 2, 3, 4, 'pg'),
+(50, 6, 'Yang berkenaan dengan pelaksanaan UJK', 0, 0, 0, 0, 'uraian'),
+(51, 7, 'Secara umum pelaksanaan pelatihan Boarding yang dilaksanakan BBPLK Semarang menurut saudara', 1, 2, 3, 4, 'pg'),
+(52, 7, 'Pelaksanaan pelatihan Boarding secara umum', 0, 0, 0, 0, 'uraian');
 
 -- --------------------------------------------------------
 
@@ -211,7 +272,8 @@ CREATE TABLE `pelatihan` (
 
 INSERT INTO `pelatihan` (`kd_pelatihan`, `id_kejuruan`, `id_program`, `tgl_mulai_pelatihan`, `tgl_akhir_pelatihan`, `tahap_pelatihan`, `kelas_pelatihan`) VALUES
 ('2019-00000001', 2, 16, '2019-12-01', '2019-12-03', 1, 1),
-('2019-00000002', 2, 16, '2019-12-01', '2019-12-03', 1, 2);
+('2019-00000002', 2, 16, '2019-12-01', '2019-12-03', 1, 2),
+('2019-00000003', 2, 16, '2019-12-06', '2019-12-17', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -247,6 +309,18 @@ CREATE TABLE `penilaian_a` (
   `id_soalA` int(11) NOT NULL,
   `jawaban` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penilaian_a`
+--
+
+INSERT INTO `penilaian_a` (`id`, `kd_pelatihan`, `id_user`, `id_soalA`, `jawaban`) VALUES
+(8, '2019-00000003', 5, 3, 1),
+(9, '2019-00000003', 5, 4, 1),
+(10, '2019-00000003', 5, 5, 2),
+(11, '2019-00000003', 6, 3, 1),
+(12, '2019-00000003', 6, 4, 2),
+(13, '2019-00000003', 6, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -312,9 +386,13 @@ CREATE TABLE `sub_soal` (
 --
 
 INSERT INTO `sub_soal` (`id_sub_soal`, `nama_sub_soal`) VALUES
-(0, 'Uraian'),
 (1, 'A'),
-(2, 'B');
+(2, 'B'),
+(4, 'Tidak Ada'),
+(5, 'C'),
+(6, 'D'),
+(7, 'E'),
+(8, 'Uraian');
 
 -- --------------------------------------------------------
 
@@ -342,7 +420,9 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `email`, `jk`, `tgl_lahir`, `pendidikan`, `pekerjaan`, `is_level`, `tipe_peserta`) VALUES
 (3, 'fajarhgl', '$2y$10$tQKN5bGmshT8aQoPnBEhwOkD/PdpYteph0XVA0dq6eHh22eACAVoe', '', 'fajarhdytt30@gmail.com', '', '0000-00-00', '', '', 1, ''),
-(4, 'fajarhgl1', '$2y$10$WD3t4nCuezWpWKtBTZyr0uX8J64USxgq3DJvqNK2vy8Od0p1CIkb2', 'Fajar Hidayat', 'fajarhdyt30@gmail.com', 'P', '2019-12-06', 'S1', 'Programmer', 0, 'Pulang');
+(4, 'fajarhgl1', '$2y$10$WD3t4nCuezWpWKtBTZyr0uX8J64USxgq3DJvqNK2vy8Od0p1CIkb2', 'Fajar Hidayat', 'fajarhdyt30@gmail.com', 'P', '2019-12-06', 'S1', 'Programmer', 0, 'Pulang'),
+(5, 'dhimas23', '$2y$10$gq4mbeMP8mAlkD1tz.obhOpO.qiqt6aCJXSAF.8to/RRtxyipdGgi', 'dhimas', 'dhimasputrabudhi@gmail.com', 'L', '2019-10-03', 'S1', 'rere', 0, 'Pulang'),
+(6, 'dhimas', '$2y$10$atOD0qvokstCIpJKPjln.uXrV1i4MfwhLrl.IehW5T.2lKnAteMjC', 'b', 'b@gmail.com', 'L', '2019-12-01', 'S1', 'nknk', 0, 'Menginap');
 
 -- --------------------------------------------------------
 
@@ -518,7 +598,7 @@ ALTER TABLE `detail_penilaian_b`
 -- AUTO_INCREMENT for table `detail_peserta`
 --
 ALTER TABLE `detail_peserta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `kejuruan`
@@ -536,13 +616,13 @@ ALTER TABLE `kuisioner_a`
 -- AUTO_INCREMENT for table `kuisioner_b`
 --
 ALTER TABLE `kuisioner_b`
-  MODIFY `id_kuisionerB` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_kuisionerB` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT for table `kuisioner_c`
 --
 ALTER TABLE `kuisioner_c`
-  MODIFY `id_kuisionerC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_kuisionerC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `pengajar`
@@ -554,7 +634,7 @@ ALTER TABLE `pengajar`
 -- AUTO_INCREMENT for table `penilaian_a`
 --
 ALTER TABLE `penilaian_a`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `penilaian_b`
@@ -578,13 +658,13 @@ ALTER TABLE `program`
 -- AUTO_INCREMENT for table `sub_soal`
 --
 ALTER TABLE `sub_soal`
-  MODIFY `id_sub_soal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_sub_soal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_token`
